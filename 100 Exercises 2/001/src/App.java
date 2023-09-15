9a36e4ae4b1bb07f52123fdfe18116558ecbf3f8
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2023-09-14 22:08:55 -0300

    001
    001

---
+++ 100 Exercises 2/001/src/App.java
@@ -0,0 +1,7 @@
+//Project main class.
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Print "Hello, World!" to the terminal window.
+        System.out.println("Hello, World!");
+    }
+}
