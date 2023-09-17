1c8392f4ea4405ad644659c0a51dda04aa5ab1d4
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2023-09-17 11:23:44 -0300

    00

--- 100 Exercises/001/src/pkg001/Main.java
+++ 100 Exercises/001/src/pkg001/Main.java
@@ -1,23 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg001;
-
-/**
- *
- * @author Juliano Silva
- */
 
 public class Main {
 
     /**
-     * @param args the command line arguments
+     * @param args
      */
-    
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hello, World!");
-    }
-    
+    }  
+
 }
--- 100 Exercises/004/src/pkg004/Main.java
+++ 100 Exercises/004/src/pkg004/Main.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg004;
 
 import java.util.Scanner;
-
-/**
- *
- * @author Juliano Silva
- */
 
 public class Main {
 
