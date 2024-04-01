e9b3973e139817efb5d90c4739c1a7a54c924933
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-01 16:45:29 -0300

    Update Main.java

--- Exercicios/005/Main.java
+++ Exercicios/005/Main.java
@@ -1,39 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package pkg005;
-
-//@author Juliano Silva
 import java.util.Scanner;
 
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // Scanner para captar o que sera digitado no teclado.
         Scanner teclado = new Scanner(System.in);
-        // Logica para obeter as notas do aluno.
         System.out.print("Aluno: ");
         String aluno = teclado.nextLine();
         System.out.print("Nota 1: ");
         double nota1 = teclado.nextDouble();
         System.out.print("Nota 2: ");
         double nota2 = teclado.nextDouble();
-        /*
-         *Logica para obter media do aluno 
-         *Operacoes no java devem seguir ordem de precedencia
-         *operacoes dentro de parenteses sao feitas antes de qualquer outra operacao.
-         */
+    
         double media = (nota1 + nota2) / 2;
-        // Logica para imprimir resultado no terminal.
+
         System.out.println("Media do Aluno(a)");
         System.out.println("Aluno: " + aluno);
         System.out.println("Media: " + media);
-
     }
-
 }
