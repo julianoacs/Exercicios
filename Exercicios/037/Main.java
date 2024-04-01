48ec7f898ad9ac17a5d94049d1252a1aa3acc0c9
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-01 13:06:22 -0300

    037
    037

---
+++ Exercicios/037/Main.java
@@ -0,0 +1,45 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner teclado = new Scanner(System.in);
+        System.out.println("Calculadora de aumento de salario.");
+        System.out.print("Nome: ");
+        String nome = teclado.nextLine();
+        System.out.print("Genero: (M) Mulher / (H) Homen: ");
+        String genero = teclado.nextLine();
+        System.out.print("Salario atual: R$ ");
+        double salario = teclado.nextDouble();
+        System.out.print("Anos de empresa: ");
+        int anosNaEmpresa = teclado.nextInt();
+
+        double novoSalario = 0;
+
+        if (genero.equalsIgnoreCase("M")){
+            if (anosNaEmpresa < 15){
+                novoSalario = salario * 0.05 + salario;
+            } else {
+                if (anosNaEmpresa <=20){
+                    novoSalario = salario * 0.12 + salario;
+                } else {
+                    novoSalario = salario * 0.23 + salario;
+                }
+            }
+        } else {
+            if (anosNaEmpresa < 20){
+                novoSalario = salario * 0.03 + salario;
+            } else {
+                if (anosNaEmpresa <=30){
+                    novoSalario = salario * 0.13 + salario;
+                } else {
+                    novoSalario = salario * 0.25 + salario;
+                }
+            }
+        }
+        System.out.println("***********************************");
+        System.out.println("Funcionario: " + nome);
+        System.out.println("Novo Salario: R$ " + novoSalario);
+        System.out.println("***********************************");
+    }
+}
