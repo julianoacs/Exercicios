652e0ccae37ddbafbcd4feb9239958a3fef9919c
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-01 13:13:10 -0300

    038
    038

---
+++ Exercicios/038/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador = 6;
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+        System.out.println(contador);
+    }
+}
