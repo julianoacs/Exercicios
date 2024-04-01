c4321189754c16abd6e345b35c474f4b576c651c
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-01 16:43:15 -0300

    003
    003

--- Exercicios/003/Main.java
+++ Exercicios/003/Main.java
@@ -1,37 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package pkg003;
-
-// @author Juliano Silva
-// Import de dependencias do Java.
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // Scanner para captar o que sera digitado no teclado.
         Scanner teclado = new Scanner(System.in);
-        // Logica para digitar o Nome e Salario.
         System.out.print("Digite seu Nome: ");
         String nome = teclado.nextLine();
         System.out.print("Salario R$ ");
         double salario = teclado.nextDouble();
 
-        // Logica para captar e formatar o mes atual do sistema.
         LocalDate dataAtual = LocalDate.now();
         String mesAtual = dataAtual.format(DateTimeFormatter.ofPattern("MMMM"));
-        
-        // Metodo para imprimir no terminal o programa solicitado.
+
         System.out.println("O funcionario " + nome + " tem um salrio de R$ " + salario + " em " + mesAtual);
-
     }
-
 }
