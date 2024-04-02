5fa230c92f92b1db7704925328a2908c4c65710d
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-02 09:50:23 -0300

    040
    040

---
+++ Exercicios/040/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador = 3;
+        while (contador <= 18){
+            System.out.println(contador);
+            contador = contador + 3;
+        }
+        System.out.println("Acabou!");
+    }
+}
