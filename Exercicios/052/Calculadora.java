59baa6f4c50c2750b9699a518f44871c1681725a
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-04 14:32:17 -0300

    052
    052

---
+++ Exercicios/052/Calculadora.java
@@ -0,0 +1,5 @@
+public class Calculadora {
+    public int dobroNumero(int numero){
+        return numero * 2;
+    }
+}
---
+++ Exercicios/052/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.dobroNumero(8);
+
+        System.out.println(resultado);
+    }
+}
