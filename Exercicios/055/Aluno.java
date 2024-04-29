b1b1028536ad59f5a7d03ecf7df8c42276255efc
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-29 09:30:00 -0300

    055
    055

---
+++ Exercicios/055/Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno {
+    String nome;
+    int idade;
+
+    void exibirInformacoes(){
+        System.out.println("Aluno.");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
---
+++ Exercicios/055/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+
+        aluno.nome = "Juliano Silva";
+        aluno.idade = 28;
+
+        aluno.exibirInformacoes();
+    }
+}
