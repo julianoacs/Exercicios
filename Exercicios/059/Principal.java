1ed99278cb07556a304ca4898fb3d9848a751086
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-04-30 09:29:09 -0300

    059
    059

---
+++ Exercicios/059/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+
+        aluno1.setNome("Juliano Silva");
+        aluno1.setNota1(10);
+        aluno1.setNota2(9);
+        aluno1.setNota3(8);
+
+        aluno1.exibirInformacoes();
+    }
+}
