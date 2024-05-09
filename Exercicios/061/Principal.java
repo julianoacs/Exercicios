11bf837b156b626f40cf3a209df4eb721e02e37a
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-05-09 11:23:09 -0300

    Update Principal.java

--- Exercicios/061/Principal.java
+++ Exercicios/061/Principal.java
@@ -1,7 +1,7 @@
 public class Principal {
     public static void main(String[] args) {
         ModeloCarro meuCarro = new ModeloCarro();
-        meuCarro.definirModelo("Fiesta ");
+        meuCarro.definirModelo("Fiesta");
         meuCarro.definirPrecos(26000, 25000 , 23500);
         meuCarro.exibirInfo();
     }
