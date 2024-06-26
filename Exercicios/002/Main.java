a4a6a46badc769d116f96c894a8d529a39815570
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-06-26 11:14:10 -0300

    Update Main.java

--- Exercicios/002/Main.java
+++ Exercicios/002/Main.java
@@ -1,11 +1,22 @@
+// Import da classe Scanner, usado para ler entradas do usuário a partir do teclado ou de outras fontes de entrada.
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        // Objeto que será usado para ler entrada do usuário.
         Scanner teclado = new Scanner(System.in);
-        System.out.println("--- Bem Vindo ---");
+        
+        // Método para imprimir a mensagem fornecida, solicitando ao usuário que digite seu nome.
         System.out.println("Digite seu nome:");
+        
+        // Método do objeto para ler a linha de entrada do usuário e armazenar na variável 'nome'
         String nome = teclado.nextLine();
+        
+        // Método para imprimir a mensagem fornecida, concatenada com o nome inserido pelo usuário
         System.out.println("Ola, seja muito bem vindo " + nome + ", é um prazer te conhecer!");
+
+        // Fechamento do objeto 'scanner'
+        teclado.close();
     }
 }
