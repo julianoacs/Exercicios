de8c52f6522119366f49cc8f8e8696ec5d8c3a52
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2024-07-09 22:50:57 -0300

    Update Main.java

--- Exercicios/005/Main.java
+++ Exercicios/005/Main.java
@@ -1,19 +1,32 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa a classe Scanner para ler a entrada do usuário.
 
 public class Main {
     public static void main(String[] args) {
+
+        // Objeto que será usado para ler entrada do usuário.
         Scanner teclado = new Scanner(System.in);
+
+        // Solicita e lê o nome do aluno.
         System.out.print("Aluno: ");
         String aluno = teclado.nextLine();
+
+         // Solicita e lê a primeira nota do aluno.
         System.out.print("Nota 1: ");
         double nota1 = teclado.nextDouble();
+
+         // Solicita e lê a segunda nota do aluno.
         System.out.print("Nota 2: ");
         double nota2 = teclado.nextDouble();
-    
-        double mediaNotas = (nota1 + nota2) / 2;
 
+        // Calcula a média das duas notas.
+        double mediaNotas = (nota1 + nota2) / 2;
+        
+        // Exibe a média do aluno.
         System.out.println("Media do Aluno(a)");
         System.out.println("Aluno: " + aluno);
         System.out.println("Media: " + mediaNotas);
+        
+        // Fecha o Scanner para liberar recursos
+        teclado.close();
     }
 }
